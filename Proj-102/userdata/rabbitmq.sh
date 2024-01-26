7ec8587267df6052519c1acbe9f0e62748e27baa
Ibidapo-chad <Ibidapo.chad@gmail.com>
2024-01-26 10:42:33 +0100

    add setup files for cloud provisioning

---
+++ Proj-102/userdata/rabbitmq.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+sudo -i
+yum install epel-release -y
+yum update -y
+yum install wget -y
+cd /tmp/
+wget http://packages.erlang-solutions.com/erlang-solutions-2.0-1.noarch.rpm
+rpm -Uvh erlang-solutions-2.0-1.noarch.rpm
+yum -y install erlang socat
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
+yum install rabbitmq-server -y
+systemctl start rabbitmq-server
+systemctl enable rabbitmq-server
+systemctl status rabbitmq-server
+sh -c 'echo "[{rabbit, [{loopback_users, []}]}]." > /etc/rabbitmq/rabbitmq.config'
+rabbitmqctl add_user test test
+rabbitmqctl set_user_tags test administrator
+systemctl restart rabbitmq-server
